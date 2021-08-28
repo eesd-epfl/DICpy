9541cfc4983cd2beee2b19288d45107917ef6cd1
Ketson R. M. dos Santos <47835396+ketsonroberto@users.noreply.github.com>
2021-08-28 14:18:17 +0200

    Update __init__.py

--- src/DICpy/__init__.py
+++ src/DICpy/__init__.py
@@ -5,12 +5,12 @@ DICpy: digital imahe correlation with python
 
 import pkg_resources
 
-import DICpy.dic2d.local
+import DICpy.dic2d
 import DICpy.pre_processing
 import DICpy.synthetic
 import DICpy.utils
 
-from DICpy.dic2d.local import *
+from DICpy.dic2d import *
 from DICpy.pre_processing import *
 from DICpy.synthetic import *
 from DICpy.utils import *
