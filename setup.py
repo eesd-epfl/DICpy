b7e1b8689617fd0f1d4354c284d050e42904d6c6
ketsonroberto <ketson.santos@gmail.com>
2021-08-31 00:42:16 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DICpy',
-    version="0.2.0",
+    version="0.3.0",
     url='https://github.com/eesd-epfl/DICpy',
     description="Digital Image Correlation with Python.",
     author="Ketson R. M. dos Santos",
